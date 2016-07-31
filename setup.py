f70bc2d394e219f9990609752cb4ff3d18ba9132
Paul Water <p.r.water@setbased.nl>
2016-07-31 23:00:36 +0200

    Release: 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='Enarksh-Lib',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='Enarksh-Lib',
     long_description=long_description,
